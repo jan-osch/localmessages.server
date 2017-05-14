4da7c41ba15a3129bb8f9da8ee9c27b2a6e5badd
janek_g <jmgrzesik@gmail.com>
2017-05-14 22:43:34 +0200

    Adds a basic dropwizard server.

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,37 @@
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class AppMain extends Application<LocationsConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new AppMain().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "app";
+    }
+
+    @Override
+    public void initialize(Bootstrap<LocationsConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(LocationsConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment
+                .healthChecks()
+                .register("template", healthCheck);
+
+        environment.jersey().register(resource);
+    }
+
+}
